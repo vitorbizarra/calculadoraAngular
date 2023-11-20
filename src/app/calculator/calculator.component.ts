8b6a14502e25ff6a425105d04dc31ebc11549259
vitorbizarra <vitorbizarracontato@gmail.com>
2023-11-20 01:54:37 -0300

    :tada: init: First Commit

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+}
